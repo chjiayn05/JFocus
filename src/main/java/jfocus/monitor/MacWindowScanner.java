6adb9c38b6e5df133ccd20da43fc719a8b297471
chjiayn05 <wtps5955linda@gmail.com>
2026-05-21 13:07:45 +0800

    fix: 修理macOS最小化視窗不算結束視窗session的問題

--- src/main/java/jfocus/monitor/MacWindowScanner.java
+++ src/main/java/jfocus/monitor/MacWindowScanner.java
@@ -11,16 +11,24 @@ public class MacWindowScanner implements WindowScanner {
     public Map<String, WindowSession> scanWindows() {
         Map<String, WindowSession> currentScan = new HashMap<>();
 
-        // 透過 AppleScript 取得 macOS 目前開啟的視窗
+        // 透過 AppleScript 取得 macOS 目前開啟的視窗 (過濾掉最小化/縮小的視窗)
         String script = "tell application \"System Events\"\n" +
                 "    set windowList to \"\"\n" +
                 "    repeat with p in (every process whose visible is true)\n" +
                 "        set pName to name of p\n" +
                 "        try\n" +
                 "            repeat with w in (every window of p)\n" +
-                "                set wName to name of w\n" +
-                "                if wName is not \"\" then\n" +
-                "                    set windowList to windowList & pName & \"::\" & wName & \"\\n\"\n" +
+                "                set isMini to false\n" +
+                "                try\n" +
+                "                    set isMini to miniaturized of w\n" +
+                "                on error\n" +
+                "                    set isMini to false\n" +
+                "                end try\n" +
+                "                if isMini is false then\n" +
+                "                    set wName to name of w\n" +
+                "                    if wName is not \"\" then\n" +
+                "                        set windowList to windowList & pName & \"::\" & wName & \"\\n\"\n" +
+                "                    end if\n" +
                 "                end if\n" +
                 "            end repeat\n" +
                 "        end try\n" +
